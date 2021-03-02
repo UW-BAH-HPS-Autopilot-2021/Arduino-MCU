8bee66a810361a183130f03aa4c89424aa7fc78c
cdeitner <71476146+cdeitner@users.noreply.github.com>
2021-03-01 19:47:42 -0800

    4 servo joystick control

--- Test_Sketch/ServoTest/ServoTest/ServoTest.ino
+++ Test_Sketch/ServoTest/ServoTest/ServoTest.ino
@@ -10,7 +10,11 @@
 #include <Servo.h>
 //hello james
 
-Servo myservo;  // create servo object to control a servo
+Servo servoL;
+Servo servoU;
+Servo servoR;
+Servo servoD;
+
 // twelve servo objects can be created on most boards
 
 int adc_max = 1024;
@@ -20,8 +24,10 @@ int joystick_y_90;
 int pos = 0;    // variable to store the servo position
 
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
-
+  servoL.attach(9);  // attaches the servo on pin 9 to the servo object
+  servoR.attach(10);  
+  servoU.attach(5);  
+  servoD.attach(6);  
   //set up adc read pins
   joystick_x_90 = analogRead(A1);
   joystick_y_90 = analogRead(A2);
@@ -41,21 +47,32 @@ void loop() {
     double x_pos; //= (analogRead(A1)/joystick_x_90) * 90; //outputs 0 to 180 depending on the joystick position
     int x_joy;
 
+    double y_pos;
+    int y_joy;
+
     for (int i = 0; i < 10; i++) {
       x_joy = (analogRead(A1));
+      y_joy = (analogRead(A2));
       if ((x_joy > joystick_x_90 + 40) || (x_joy < joystick_x_90 - 40)) {
         x_pos = x_pos + ((x_joy*900)/joystick_x_90);
       } else { 
         x_pos = x_pos + (900);
       }
+
+      if ((y_joy > joystick_y_90 + 40) || (y_joy < joystick_y_90 - 40)) {
+        y_pos = y_pos + ((y_joy*900)/joystick_y_90);
+      } else { 
+        y_pos = y_pos + (900);
+      }
     }
     x_pos = x_pos/100;
+    y_pos = y_pos/100;
 
-    //int y_pos = ((analogRead(A2) * 90) / joystick_y_90);
-        
-    myservo.write(x_pos);
-    
-
+            
+    servoL.write(x_pos);
+    servoR.write(180 - x_pos);
+    servoU.write(y_pos);
+    servoD.write(180 - y_pos);
 
 
 }
