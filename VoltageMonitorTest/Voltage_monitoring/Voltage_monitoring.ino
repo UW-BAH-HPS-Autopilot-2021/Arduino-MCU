e030dab1b77abfd6b082f3c3180e59c9acf20088
James <77418664+jdelee33@users.noreply.github.com>
2021-05-09 18:45:44 -0700

    Update Voltage_monitoring.ino
    Adjusted voltage divider values and offset for better tracking with new PCB.

--- VoltageMonitorTest/Voltage_monitoring/Voltage_monitoring.ino
+++ VoltageMonitorTest/Voltage_monitoring/Voltage_monitoring.ino
@@ -1,6 +1,6 @@
 // Modified "ReadAnalogVoltage" sketch in public domain: http://www.arduino.cc/en/Tutorial/ReadAnalogVoltage
 // Modified by: James Lee
-// Modify Date: 4/22/2021
+// Modify Date: 5/9/2021
 
 // *******
 // GLOBALS
@@ -28,16 +28,16 @@ void loop() {
   int V_3V3_READ = analogRead(A3);
   
   // Convert the integer analog input (which goes from 0 - 1023) to input voltage at pin:
-  // 0.03 offset based on pin measurements with multimeter
-  float V_BATT_IN = 0.03 + V_BATT_READ * 3.3 / 1023.0;
-  float V_5V_IN = 0.03 + V_5V_READ * 3.3 / 1023.0;
-  float V_3V3_IN = 0.03 + V_3V3_READ * 3.3 / 1023.0;
+  // 0.05 offset based on no-load pin measurements with multimeter
+  float V_BATT_IN = 0.05 + V_BATT_READ * 3.3 / 1023.0;
+  float V_5V_IN = 0.05 + V_5V_READ * 3.3 / 1023.0;
+  float V_3V3_IN = 0.05 + V_3V3_READ * 3.3 / 1023.0;
   
   // Convert the pin voltage to original voltage based on measured divider values.
-  // General formula: V = V_IN * (R1 + R2) / R1
-  float V_BATT = V_BATT_IN * (103e3 + 22.5e3) / 22.5e3;
-  float V_5V = V_5V_IN * (104e3 + 47.2e3) / 47.2e3;
-  float V_3V3 = V_3V3_IN * (102e3 + 47.3e3) / 47.3e3;
+  // General formula: V = V_IN * (R_TOP + R_BOTTOM) / R_BOTTOM
+  float V_BATT = V_BATT_IN * (122.5e3 / 22e3);
+  float V_5V = V_5V_IN * (132.5e3 / 33e3);
+  float V_3V3 = V_3V3_IN * (146.5e3 / 47e3);
   
   // print to serial in .csv format
   // output can be copied from the serial monitor to create a .csv file
@@ -54,5 +54,5 @@ void loop() {
   Serial.print(V_5V);
   Serial.print(",");
   Serial.println(V_3V3);
-  delay(60000);                         // Delay 1 minute before next loop
+  delay(60000);                         // Adjust time delay for desired log frequency
 }
