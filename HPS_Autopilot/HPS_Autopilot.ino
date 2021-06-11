064ba644afeb9a9e2e9f52e8f1149fc5278aba2e
cdeitner <71476146+cdeitner@users.noreply.github.com>
2021-06-11 12:58:06 -0700

    Update HPS_Autopilot.ino

--- HPS_Autopilot/HPS_Autopilot.ino
+++ HPS_Autopilot/HPS_Autopilot.ino
@@ -28,7 +28,7 @@ U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 // End of constructor list
 
 
-int myLed  = 11;
+int myLED  = 11;
 
 //Joystick Variables
 Servo servoL;
